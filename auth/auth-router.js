ac7c2394c557a1b5af643bbc41e782edc4bb1c0e
Alex Kemper <alexkemper@MacBook-Pro.local>
2020-08-18 12:52:17 -0400

    register user and hash encryption are working

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -7,4 +7,29 @@ router.get("/", (req, res) => {
   res.status(200).json({ message: "auth router reached" });
 });
 
+router.get("/users", (req, res) => {
+  Users.find({})
+    .then((response) => {
+      res.status(200).json({ data: response });
+    })
+    .catch((error) => {
+      res.status(500).json({ error: error.message });
+    });
+});
+
+router.post("/register", (req, res) => {
+  const { username, password } = req.body;
+
+  const rounds = process.env.HASH_ROUNDS || 8;
+  const hash = bcrypt.hashSync(password, rounds);
+
+  Users.add({ username, password: hash })
+    .then((user) => {
+      res.status(201).json({ data: user });
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err.message });
+    });
+});
+
 module.exports = router;
